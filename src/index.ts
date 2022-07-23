2075723ff84f542503ff53e86067bebe973280d0
Rodrigo <rodrigorael53@gmail.com>
2022-07-23 17:34:53 -0300

    get the number of access

--- src/index.ts
+++ src/index.ts
@@ -17,10 +17,27 @@ export default {
 			token: env.UPSTASH_REDIS_REST_TOKEN
 		});
 
-		const access_count = await redis.incr('access_count');
+		if (request.url.endsWith('/get')) {
+			// buscando(fazendo uma leitura) o número de acessos
+			const access_count = await redis.get('access_count');
 
-		return new Response(JSON.stringify({
-			access_count
-		}));
+			return new Response (JSON.stringify({
+				access_count
+			}));
+		} else {
+			// o contador está fazendo uma operação par alterar os dados no banco
+			// e como eu estou fazendo uma operação de escrita(mudando uma informação)
+			// eu não estou utilizando o conceito de Edge no banco de dados porque sempre
+			// que é feito operações de escrita, os dados sempre vão ser alterados(escritos)
+			// em uma única região do mundo
+			// utilizando o conceito de edge-computing no upstash com redis é somente
+			// para leitura de dados que a latência é muito pequena pois eu vou lêr os
+			// dados da região mais próxima de mim.
+			const access_count = await redis.incr('access_count');
+
+			return new Response(JSON.stringify({
+				access_count
+			}));
+		}
 	},
 };
